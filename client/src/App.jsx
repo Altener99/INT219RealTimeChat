6c3b4088ba0f59f85dc82a58a38156fc4597811b
Devanshu Dutta <108408165+Altener99@users.noreply.github.com>
2024-04-19 14:08:34 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Routes from "./Routes";
 
 function App() {
 //change the baseURL to the server's URL with a slash at the end
-  axios.defaults.baseURL = 'http://eternity-chat.onrender.com/';
+  axios.defaults.baseURL = 'https://eternity-chat.onrender.com/';
   axios.defaults.withCredentials = true;
   
   
